2d3ee6e6b8ee963ad4706138e8a9fcdcb1cf7caf
An-Fruit <92803192+An-Fruit@users.noreply.github.com>
2022-02-10 09:19:52 -0600

    Add files via upload

--- QuestionsGame.java
+++ QuestionsGame.java
@@ -1,4 +1,7 @@
 import java.util.*;
+
+import BinarySearchTreeLab.Node;
+
 import java.io.*;
 
 // This is a starter file for QuestionsGame.
@@ -10,22 +13,61 @@ public class QuestionsGame {
    //global variables
 	private QuestionNode root;
 	
+	String curr;
 	
 	public QuestionsGame() {
 		
 	}
+	
+	private void preOrder(QuestionNode n) { 
+		if(n != null) {
+			
+			preOrder(n.left);
+			preOrder(n.right);
+		}
+		return;
+	}
+	
 	public void read(String filename) throws Exception{
 		Scanner file = new Scanner(new File(filename));
-		//checks for question/asnwer
 		
+		//first 2 lines are always root node
+		file.nextLine().trim();
+		root.data = file.nextLine().trim();
+		
+		//checks for question/answer
+		
+		QuestionNode temp = root;
+		while(temp.left != null)
 		while(file.hasNext()) {
 			String status = file.nextLine().trim();
 			String words = file.nextLine().trim();
-			if(status.equals("Q:")) root.data = words;
+			add(status + " " + words);
+			
 		}
 		
 		file.close();
 	}
+	
+	private void add(String o)
+	{
+		root = add( o, root );		
+	}
+	private QuestionNode add( String o, QuestionNode n )
+	{
+		if(n == null) {
+			n = new QuestionNode(o, null, null);
+		}
+		if(n.left != null && n.left.data.substring(0,2).equals("A:")) {
+			n.right = add(o, n.right);
+		}
+		else {
+			n.left = add(o, n.left);
+		}
+		
+		return n;
+	}
+	
 	
 	public boolean yesTo(String prompt) {
 		Scanner input = new Scanner(System.in);
